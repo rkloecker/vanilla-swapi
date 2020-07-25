290871adfa56a6097516e3d5faf69642c08e3f9d
R K <blah@eample.com>
2020-07-25 11:14:24 +0200

    replaced id with data-attribute

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const url = "https://swapi.dev/api/people/";
 const output = document.querySelector("#output");
 const moreBtn = document.querySelector("#showMore");
-const goBackBtn = document.querySelector("#goback");
+// const goBackBtn = document.querySelector("#goback");
 const rw = document.querySelector("#rw");
 const dataArray = [];
 
@@ -11,21 +11,21 @@ function showResult(url) {
   fetch(url)
     .then((resp) => resp.json())
     .then((data) => {
-      dataArray.push(...data.results);
-      show(dataArray);
+      // dataArray.push(...data.results);
+      show(data.results);
     });
 }
 
-function show(el) {
-  console.log(el);
+function show(arr) {
+  console.log(arr);
 
-  el.forEach(({ name, mass, height, birth_year }) => {
+  arr.forEach(({ name, mass, height, birth_year }) => {
     const item = output.content.cloneNode(true);
 
-    item.querySelector(`#name`).innerText = ` ${name}: `;
-    item.querySelector("#mass").innerText = ` ${mass}`;
-    item.querySelector("#height").innerText = ` ${height}`;
-    item.querySelector("#birthyear").innerText = ` ${birth_year}`;
+    item.querySelector("[data-name]").innerText = name;
+    item.querySelector("[data-mass]").innerText = mass;
+    item.querySelector("[data-height]").innerText = height;
+    item.querySelector("[data-birthyear]").innerText = birth_year;
     rw.append(item);
   });
 
@@ -34,12 +34,13 @@ function show(el) {
 function addContent(num) {
   cnt += num;
   showResult(`https://swapi.dev/api/people/?page=${cnt}`);
-  if (cnt > 1) {
-    goBackBtn.style.display = "block";
-  } else {
-    goBackBtn.style.display = "none";
-  }
+  // if (cnt > 1) {
+  //   goBackBtn.style.display = "block";
+  // }
+  // else {
+  //   goBackBtn.style.display = "none";
+  // }
 }
 window.onload = showResult(url);
 moreBtn.addEventListener("click", () => addContent(1));
-goBackBtn.addEventListener("click", () => show(dataArray.slice(0, 8)));
+// goBackBtn.addEventListener("click", () => show(dataArray.slice(0, 8)));
